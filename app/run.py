d5270456cf2ddfc0b585fe95323843f31f71db61
Chloe Skorpikova <cskorpi@wgu.edu>
2022-07-11 14:15:03 -0700

    Query all books from database and display on homepage

--- app/run.py
+++ app/run.py
@@ -1,6 +1,9 @@
-from app import create_app
+from app import create_app, db
 
 if __name__ == '__main__':
     print('Attempting to run app...')
     flask_app = create_app('dev')
+    with flask_app.app_context():
+        db.create_all()
     flask_app.run()
+
