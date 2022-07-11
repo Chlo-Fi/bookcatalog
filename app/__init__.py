33f802ddf17f28123ac8180b0987d4eb8e93e3aa
Chloe Skorpikova <cskorpi@wgu.edu>
2022-07-11 15:13:14 -0700

    Added styles.css, layout.html and extended template to all html pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 #app\__init__.py
-
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 db = SQLAlchemy()
+bootstrap = Bootstrap()
 
 def create_app(config_type): # dev, test, or prod
     app = Flask(__name__)
@@ -12,8 +13,10 @@ def create_app(config_type): # dev, test, or prod
     configuration = os.path.join(os.getcwd(), 'config', config_type + '.py')
     app.config.from_pyfile(configuration)
 
-    print('Initializing database...')
+    print('Binding database to app...')
     db.init_app(app)
+    print('Initializing bootstrap...')
+    bootstrap.init_app(app)
 
     from app.catalog import main
     app.register_blueprint(main)
