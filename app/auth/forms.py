482be86bff676d127af038ccc7c6363622f69e09
Chloe Skorpikova <cskorpi@wgu.edu>
2022-07-11 16:43:15 -0700

    Added data validation to registration form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
 
 class RegistrationForm(FlaskForm):
-    name = StringField("What is your name?")
-    email = StringField("Enter you email address:")
+    name = StringField("Name", validators=[DataRequired(), Length(3,15, message='Name must be between 3 and 15 characters.')])
+    email = StringField("Email Address", validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(15), EqualTo('confirm', message="Passwords do not match.")])
+    confirm = PasswordField('Confirm', validators=[DataRequired()])
     submit = SubmitField('Register')
